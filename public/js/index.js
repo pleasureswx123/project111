ecef74b7c335eb3ea79deba7ec0012d1a008b6df
pleasureswx123 <pleasureswx123@163.com>
2016-05-05 12:10:05 +0800

    测试git客户端

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import {Provider,connect } from 'react-redux';
 import App from './containers/app';
 
+//test
 
 import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './constants/actionTypes'
 
